89787d3f09b3e4e47c204f8ba58f96dfdde8ea65
Abhinandh S <ugabhi@proton.me>
2024-11-08 12:17:03 +0530

    Refactoring

---
+++ test/test_file.tsx
@@ -0,0 +1,11 @@
+// NOTE: emmet langauge server is working fine 
+export default function Home() {
+  return(
+    <div id="wrapper" className="font-bold">
+    <p></p>
+    </div>
+    <div id="wrapper" className="font-bold"></div>
+    <div id="wrapper" className="font-bold"></div>
+    <div id="wrapper" className="font-bold"></div>
+  );
+}
