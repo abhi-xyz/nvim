54501254fcddf8c2de5291054c6dbefa64aa1b0b
Abhinandh S <ugabhi@proton.me>
2024-12-01 12:05:57 +0530

    added more keymaps

--- lua/configs/remap.lua
+++ lua/configs/remap.lua
@@ -6,8 +6,30 @@ keymap("v", "j", ":m '>+1<CR>gv=gv", { desc = "move selected item up and down" }
 keymap("v", "k", ":m '<-2<CR>gv=gv", { desc = "move selected item up and down" })
 keymap("x", "<leader>p", "\"_dp", { desc = "paste without losing" } )
 keymap("n", "Q", "<nop>", { desc = "" })
--- Duplicate a line
 keymap('n', '<leader>y', ':t.<CR>', { desc = "Duplicate line" })
+keymap('n', '<leader>r', ':e!<CR>', { desc = "Reload file" })
+keymap('n', '<leader>ih', ':nohlsearch<CR>', { desc = "Clear highlights" })
+keymap('n', '<leader>wr', ':%s/\\<<C-r><C-w>\\>//gI<Left><Left><Left>', { desc = "Replace current word" })
+keymap('n', '<leader>j', 'mzJ`z', { desc = "Join lines without moving the cursor" })
+keymap('n', '<leader>ot', ':split | terminal<CR>', { desc = "Open terminal" })
+keymap('n', '<leader>d', '"_d', { desc = "Delete without copying" })
+keymap('v', '<leader>d', '"_d', { desc = "Delete selection without copying" })
+
+
+-- Switch two characters (transpose)
+vim.keymap.set('n', 'gx', 'xp', { desc = "Transpose characters" })
+
+-- Uppercase a word
+vim.keymap.set('n', '<leader>U', 'viwU<Esc>', { desc = "Uppercase current word" })
+
+-- Lowercase a word
+vim.keymap.set('n', '<leader>u', 'viwu<Esc>', { desc = "Lowercase current word" })
+
+-- Surround a word with quotes (requires vim-surround or nvim-surround)
+vim.keymap.set('n', '<leader>"', 'viw<esc>a"<esc>bi"<esc>lel', { desc = "Surround word with quotes" })
+
+-- Change word under cursor (stay in normal mode)
+vim.keymap.set('n', 'cw', '"_ciw', { desc = "Change word under cursor without yanking" })
 
 -- window management
 keymap("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })     -- split window vertically
@@ -37,5 +59,4 @@ keymap("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })         
 keymap("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })                 --  go to previous tab
 keymap("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
--- Quickly reload the current file
-keymap('n', '<leader>r', ':e!<CR>', { desc = "Reload file" })
+
