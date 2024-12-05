7bccc7f6a35eb879aa288e6cfe864c24d61e1d4e
Abhinandh S <ugabhi@proton.me>
2024-12-05 10:52:05 +0530

    more keybindings

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,5 +4,7 @@ return {
   cmd = { "Git", "G" },
   keys = {
     { "<leader>ga", ":Git add -A<CR>", desc = "Git add all changes" }, -- Keybinding for Git add -A
+    { "<leader>gc", ":Git commit<CR>", desc = "Git commit" },
+    { "<leader>gp", ":Git push<CR>", desc = "Git push" },
   },
 }
